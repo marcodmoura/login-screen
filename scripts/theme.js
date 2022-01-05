ea1e84b7774a0259f51f7d9125486fa45f023169
Marco <marcosmourabusiness@gmail.com>
2022-01-05 17:30:21 -0300

    update

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,11 @@
-const container = document.querySelector("#container");
+const container = document.querySelector("form");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
-const background = document.querySelector("body");
+const background_color = document.querySelector("body");
 const button = document.querySelector("#submit-button");
 
 function darkMode() {
+    background_color.style.background = "linear-gradient(68.15deg, #000000 16.62%, #5e4641 85.61%)";
     container.style.background = "rgba(0,0,0,0.13)";
     username.style.background = "rgba(0,0,0,0.13)";
     password.style.background = "rgba(0,0,0,0.13)";
@@ -18,6 +19,7 @@ function darkMode() {
 }
 
 function lightMode() {
+    background_color.style.background = "linear-gradient(68.15deg, #2F2325 16.62%, #8E5D52 85.61%)";
     container.style.background = "rgba(255,255,255,0.13)";
     username.style.background = "rgba(255,255,255,0.13)";
     password.style.background = "rgba(255,255,255,0.13)";
